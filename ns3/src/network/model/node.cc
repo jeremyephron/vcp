95784531b194d3905c072b791fada53e424c4f2d
Mathew Hogan <mathewhogan@DNa1da775.SUNet>
2021-05-21 19:55:46 -0700

    Uncommenting problem line 2

--- ns3/src/network/model/node.cc
+++ ns3/src/network/model/node.cc
@@ -226,7 +226,7 @@ Node::track_queue_sizes() {
       }
 
       Ptr<TrafficControlLayer> tc = GetObject<TrafficControlLayer>();
-      //Ptr<QueueDisc> qd = tc->GetRootQueueDiscOnDevice(m_devices[i]);
+      Ptr<QueueDisc> qd = tc->GetRootQueueDiscOnDevice(m_devices[i]);
       //uint32_t cur_size = qd->GetNPackets();
       //queue_sizes.push(cur_size);
       //m_persistent_queue_size_sums[i] += cur_size; 
