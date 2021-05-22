20709be63fbe9dc203f24671bdc082ea0341594c
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-21 23:06:55 -0700

    vcp

--- ns3/src/internet/model/vcp.cc
+++ ns3/src/internet/model/vcp.cc
@@ -68,6 +68,7 @@ Vcp::PktsAcked(Ptr<TcpSocketState> tcb, uint32_t segmentsAcked, const Time &rtt)
 
   // Update load state
   m_loadState = (LoadState_t)tcb->m_ectCodePoint;
+  m_loadState = LOAD_LOW;
   NS_LOG_DEBUG(tcb->m_ectCodePoint);
 
   if (!m_cWndFractionalInit) {
