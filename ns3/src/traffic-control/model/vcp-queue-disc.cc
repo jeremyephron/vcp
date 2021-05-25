c205a9e3c60578be34c551ec4f949e16bb76fc00
Mathew Hogan <mathewhogan@DNa1da775.SUNet>
2021-05-25 16:33:22 -0700

    Add QueueDisc for VCP

---
+++ ns3/src/traffic-control/model/vcp-queue-disc.cc
@@ -0,0 +1,142 @@
+#include "vcp-queue-disc.h"
+#include "ns3/simulator.h"
+#include "ns3/vcp-packet-tag.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE("VcpQueueDisc");
+
+NS_LOG_ENSURE_REGISTERED(VcpQueueDisc);
+
+TypeId VcpQueueDisc::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::VcpQueueDisc")
+    .SetParent<QueueDisc> ()
+    .SetGroupName("TrafficControl")
+    .AddConstructor<VcpQueueDisc> ()
+    .AddAttribute ("MaxSize",
+                   "The maximum number of packets accepted by this queue disc",
+                   QueueSizeValue (QueueSize ("25p")),
+                   MakeQueueSizeAccessor (&QueueDisc::SetMaxSize,
+                                          &QueueDisc::GetMaxSize),
+                   MakeQueueSizeChecker ())
+    .AddAttribute ("LinkBandwidth", 
+                   "The VCP link bandwidth",
+                   DataRateValue (DataRate ("1.5Mbps")),
+                   MakeDataRateAccessor (&VcpQueueDisc::m_linkBandwidth),
+                   MakeDataRateChecker ())
+    .AddAttribute ("TimeInterval", 
+                   "Interval over which to sample load factor",
+                   TimeValue (MilliSeconds (200)),
+                   MakeTimeAccessor (&VcpQueueDisc::m_timeInterval),
+                   MakeTimeChecker ())
+     .AddAttribute ("SampleInterval", 
+                   "Interval over which to sample load factor",
+                   TimeValue (MilliSeconds (10)),
+                   MakeTimeAccessor (&VcpQueueDisc::m_QueueSampleInterval),
+                   MakeTimeChecker ())
+     .AddAttribute ("K_q",
+                   "K_q factor used while calculating load factor",
+                   DoubleValue (0.5),
+                   MakeDoubleAccessor (&VcpQueueDisc::m_kq),
+                   MakeDoubleChecker<double> ())
+    .AddAttribute ("TargetUtil",
+                   "Target utilization of link capacity",
+                   DoubleValue(1.0),
+                   MakeDoubleAccessor (&VcpQueueDisc::m_target_util),
+                   MakeDoubleChecker<double> ()) 
+  ;
+
+  return tid;
+}
+
+VcpQueueDisc::VcpQueueDisc () :
+    FifoQueueDisc ()
+{
+  NS_LOG_FUNCTION (this);
+  m_queue_size_sample_timer.SetFunction(&VcpQueueDisc::SampleQueueSize, this);
+  m_queue_size_sample_timer.Schedule(m_QueueSampleInterval);
+}
+
+VcpQueueDisc::~VcpQueueDisc ()
+{
+    NS_LOG_FUNCTION (this);
+}
+
+void
+VcpQueueDisc::DoDispose (void)
+{
+  NS_LOG_FUNCTION (this);
+  QueueDisc::DoDispose ();
+}
+
+bool
+VcpQueueDisc::DoEnqueue (Ptr<QueueDiscItem> item)
+{
+  NS_LOG_FUNCTION (this << item);
+
+  // add to recent arrivals queue
+  now = Simulator::Now();
+  recent_packet_arrivals.push(now);
+
+  // remove old (no longer recent arrivals)
+  while (!recent_packet_arrivals.empty() &&
+          (now.ToInteger(Time::Unit::MS) -
+           recent_packet_arrivals.front().ToInteger(Time::Unit::MS) > 
+           m_timeInterval.ToInteger(Time::Unit::MS))
+  {
+    recent_packet_arrivals.pop();
+  }
+
+  double persist_q_size = (double) m_qsizes_sum / recent_queue_sizes.size ();
+  int recent_arrivals = recent_packet_arrivals.size ();
+
+  double load_factor = recent_arrivals + m_kq * persist_q_size /
+                       ((m_target_util * m_linkBandwith.Get ().GetBitRate () / 1000 * 8) *
+                        (m_timeInterval.ToInteger(Time::Unit::MS) / 1000));
+  
+  VcpPacketTag vcpTag;
+  if (load_factor < 0.8) {
+    vcpTag.setLoad (VcpPacketTag::LoadType::LOAD_LOW);
+  } else if (load_factor < 1) {
+    vcpTag.setLoad (VcpPacketTag::LoadType::LOAD_HIGH);
+  } else {
+    vcpTag.setLoad (VcpPacketTag::LoadType::LOAD_OVERLOAD);
+  }
+
+  item->GetPacket ()->AddPacketTag (vcpTag);
+ 
+  FifoQueueDisc::DoEnqueue(item);
+}
+
+void
+VcpQueueDisc::InitializeParams (void)
+{
+  NS_LOG_FUNCTION (this);
+  
+  m_qsizes_sum = 0;
+  recent_queue_sizes.push(0); // add one initial queue size sample
+}
+
+void
+VcpQueueDisc::SampleQueueSize()
+{
+  if (recent_queue_sizes.size () >=
+          m_timeInterval.ToInteger(Time::Unit::MS) /
+          m_QueueSampleInterval.ToInteger(Time::Unit::MS))
+  {
+    m_qsizes_sum -= recent_queue_sizes.front ();
+    recent_queue_sizes.pop ();
+  }
+
+  uint32_t cur_size = GetCurrentSize ().GetValue ();
+  recent_queue_sizes.push (cur_size);
+  m_qsizes_sum += cur_size;
+
+  m_queue_size_sample_timer.SetFunction(&VcpQueueDisc::SampleQueueSize, this);
+  m_queue_size_sample_timer.Cancel();
+  m_queue_size_sample_timer.Schedule(m_QueueSampleInterval);
+}
+
+} // namespace ns3
+
