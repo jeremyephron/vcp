cbde1847f604743dae05717f854b18430cbe04d2
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-26 00:23:06 -0700

    vcp

--- ns3/scratch/my-example.cc
+++ ns3/scratch/my-example.cc
@@ -365,7 +365,7 @@ main (int argc, char *argv[])
   // DONE: Read documentation for BulkSendHelper to figure out the name of the
   //       Attribute for setting the destination address for the sender.
   ftp.SetAttribute ("Remote", receiverAddress);
-  ftp.SetAttribute ("SendSize", UintegerValue (tcpSegmentSize));
+  ftp.SetAttribute ("SendSize", UintegerValue (500 /*tcpSegmentSize*/));
 
   // DONE: Install the source application on the correct host.
   ApplicationContainer sourceApp1 = ftp.Install (h1);
