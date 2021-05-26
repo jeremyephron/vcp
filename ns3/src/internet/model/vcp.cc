47cde5f55dbc6ce722eabe28df15beeb24764db4
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-26 11:53:21 -0700

    vcp comment comment out newreno in vcp

--- ns3/src/internet/model/vcp.cc
+++ ns3/src/internet/model/vcp.cc
@@ -43,7 +43,7 @@ Vcp::IncreaseWindow(Ptr<TcpSocketState> tcb, uint32_t segmentsAcked)
 {
   // If the load bits are not supported, fall back to TCP New Reno
   if (m_loadState == LOAD_NOT_SUPPORTED) {
-    TcpNewReno::IncreaseWindow(tcb, segmentsAcked);
+    // TcpNewReno::IncreaseWindow(tcb, segmentsAcked);
   } else {
     // Do nothing. Window is increased when receiving an ACK. See PktsAcked()
   }
