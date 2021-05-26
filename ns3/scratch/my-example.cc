8569d9fc3a474d457a7340c1e080d365a3c4a526
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-26 10:07:11 -0700

    vcp

--- ns3/scratch/my-example.cc
+++ ns3/scratch/my-example.cc
@@ -384,7 +384,7 @@ main (int argc, char *argv[])
   Simulator::Schedule (Seconds (TRACE_START_TIME), &TraceRtt, rttStream);
   //Simulator::Schedule (Seconds (TRACE_START_TIME), &TraceRtt2, rttStream2);
 
-  auto m = Config::LookupMatches("/NodeList/1");
+  auto m = Config::LookupMatches("/NodeList/1/$ns3::TcpL4Protocol");
   NS_LOG_DEBUG("MATCH " << m.GetN());
   NS_LOG_DEBUG("MATCH " << m.Get(0)->GetInstanceTypeId());
 
