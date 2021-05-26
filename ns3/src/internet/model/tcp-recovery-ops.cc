b2f8869b0a36d3f1f462c0e460755e476662a86c
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-26 10:26:54 -0700

    vcp

--- ns3/src/internet/model/tcp-recovery-ops.cc
+++ ns3/src/internet/model/tcp-recovery-ops.cc
@@ -100,8 +100,7 @@ TcpClassicRecovery::EnterRecovery (Ptr<TcpSocketState> tcb, uint32_t dupAckCount
   NS_LOG_FUNCTION (this << tcb << dupAckCount << unAckDataCount);
   NS_UNUSED (unAckDataCount);
   NS_UNUSED (deliveredBytes);
-  // (VCP): TODO: is this safe
-  // tcb->m_cWnd = tcb->m_ssThresh;
+  tcb->m_cWnd = tcb->m_ssThresh;
   tcb->m_cWndInfl = tcb->m_ssThresh + (dupAckCount * tcb->m_segmentSize);
 }
 
