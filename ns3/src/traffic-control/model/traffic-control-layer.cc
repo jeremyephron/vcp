39228f3d8331cb3bd511ef96c8da2bc28576a8ca
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-26 00:08:27 -0700

    vcp

--- ns3/src/traffic-control/model/traffic-control-layer.cc
+++ ns3/src/traffic-control/model/traffic-control-layer.cc
@@ -319,7 +319,7 @@ TrafficControlLayer::Receive (Ptr<NetDevice> device, Ptr<const Packet> p,
   bool found = false;
 
   Ipv4Header ipHeader;
-  packet->PeekHeader(ipHeader);
+  p->PeekHeader(ipHeader);
   NS_LOG_DEBUG("(VCP) ip header ecn=" << ipHeader.GetEcn()); // TODO: delete
 
   for (ProtocolHandlerList::iterator i = m_handlers.begin ();
