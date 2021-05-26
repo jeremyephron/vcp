ab848f0a687f70516745824da1faf88c4e5a3c79
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-26 02:35:34 -0700

    vcp

--- ns3/src/internet/model/vcp.cc
+++ ns3/src/internet/model/vcp.cc
@@ -65,6 +65,7 @@ void
 Vcp::PktsAcked(Ptr<TcpSocketState> tcb, uint32_t segmentsAcked, const Time &rtt)
 {
   NS_LOG_FUNCTION(this << tcb << segmentsAcked << rtt);
+  tcb->m_cWnd = 14580*15;
   return;
 
   // Update load state
