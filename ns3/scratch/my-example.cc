4311e4e43701d0d9af0c085dfad7fa44f9c00ae1
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-27 15:59:33 -0700

    vcp

--- ns3/scratch/my-example.cc
+++ ns3/scratch/my-example.cc
@@ -254,7 +254,7 @@ main (int argc, char *argv[])
   Config::SetDefault ("ns3::TcpSocketBase::Sack", BooleanValue (false));
   Config::SetDefault ("ns3::TcpL4Protocol::RecoveryType",
                       TypeIdValue (TypeId::LookupByName ("ns3::TcpClassicRecovery")));
-  Config::SetDefault ("ns3::TcpSocketBase::DelAckCount", UintegerValue (0)); 
+  Config::SetDefault ("ns3::TcpSocket::DelAckCount", UintegerValue (0)); 
 
   // Select TCP variant
   TypeId tcpTid;
