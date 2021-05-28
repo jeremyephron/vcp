7d1ae02450eac7addaa94b98df6b4e33b2ef77fc
Jeremy Ephron <jeremyephron@gmail.com>
2021-05-28 11:30:45 -0700

    initial cwnd = 1 packet

--- ns3/src/internet/model/vcp.cc
+++ ns3/src/internet/model/vcp.cc
@@ -74,67 +74,70 @@ Vcp::GetSsThresh (Ptr<const TcpSocketState> state, uint32_t bytesInFlight)
 void
 Vcp::IncreaseWindow(Ptr<TcpSocketState> tcb, uint32_t segmentsAcked)
 {
-  // NS_LOG_FUNCTION(this << tcb << segmentsAcked);
+  // (VCP) TODO: potential place to do CC
+  return;
 
-  // Time rtt = tcb->m_lastRtt.Get();
+  NS_LOG_FUNCTION(this << tcb << segmentsAcked);
 
-  // NS_LOG_DEBUG("tcb->m_cWnd=" << tcb->m_cWnd);
-  // // tcb->m_cWnd = 14580;
-  // // return;
+  Time rtt = tcb->m_lastRtt.Get();
 
-  // // Update load state
-  // m_loadState = (LoadState_t)tcb->m_vcpLoad;
-  // NS_LOG_DEBUG("(VCP) m_loadState=" << m_loadState);
+  NS_LOG_DEBUG("tcb->m_cWnd=" << tcb->m_cWnd);
+  // tcb->m_cWnd = 14580;
+  // return;
 
-  // if (!m_cWndFractionalInit) {
-  //   m_cWndFractional = static_cast<double>(tcb->m_cWnd);
-  //   m_cWndFractionalInit = true;
-  // }
+  // Update load state
+  m_loadState = (LoadState_t)tcb->m_vcpLoad;
+  NS_LOG_DEBUG("(VCP) m_loadState=" << m_loadState);
 
-  // // If the load bits are not supported, fall back to TCP New Reno
-  // if (m_loadState == LOAD_NOT_SUPPORTED) {
-  //   // TODO: What to do if not supported?
-  //   // TcpNewReno::PktsAcked(tcb, segmentsAcked, rtt);
-  //   // m_cWndFractional = static_cast<double>(tcb->m_cWnd);
-  //   return;
-  // }
+  if (!m_cWndFractionalInit) {
+    m_cWndFractional = static_cast<double>(tcb->m_cWnd);
+    m_cWndFractionalInit = true;
+  }
 
-  // // Update RTT
-  // m_lastRtt = rtt.GetMilliSeconds();
+  // If the load bits are not supported, fall back to TCP New Reno
+  if (m_loadState == LOAD_NOT_SUPPORTED) {
+    // TODO: What to do if not supported?
+    // TcpNewReno::PktsAcked(tcb, segmentsAcked, rtt);
+    // m_cWndFractional = static_cast<double>(tcb->m_cWnd);
+    return;
+  }
 
-  // // Freeze cwnd after MD
-  // if (m_mdFreeze && m_mdTimer.IsRunning()) {
-  //   NS_LOG_DEBUG("(VCP) freezing cwnd after MD");
-  //   return;
-  // } else if (m_mdFreeze && m_mdTimer.IsExpired()) {
-  //   m_mdFreeze = false;
-  //   m_mdTimer.SetFunction(&Vcp::Noop, this);
-  //   m_mdTimer.Schedule(rtt);
-  // }
+  // Update RTT
+  m_lastRtt = rtt.GetMilliSeconds();
 
-  // // Perform AI for one RTT after 
-  // if (!m_mdFreeze && m_mdTimer.IsRunning()) {
-  //   AdditiveIncrease(tcb);
-  //   return;
-  // } 
+  // Freeze cwnd after MD
+  if (m_mdFreeze && m_mdTimer.IsRunning()) {
+    NS_LOG_DEBUG("(VCP) freezing cwnd after MD");
+    return;
+  } else if (m_mdFreeze && m_mdTimer.IsExpired()) {
+    m_mdFreeze = false;
+    m_mdTimer.SetFunction(&Vcp::Noop, this);
+    m_mdTimer.Schedule(rtt);
+  }
 
-  // switch (m_loadState) {
-  //   case LOAD_LOW:
-  //     MultiplicativeIncrease(tcb);
-  //     break;
-  //   case LOAD_HIGH:
-  //     AdditiveIncrease(tcb);
-  //     break;
-  //   case LOAD_OVERLOAD:
-  //     MultiplicativeDecrease(tcb);
-  //     m_mdFreeze = true;
-  //     m_mdTimer.SetFunction(&Vcp::Noop, this);
-  //     m_mdTimer.Schedule(Time(m_estInterval * 1000000));
-  //     return;
-  //   default:
-  //     NS_LOG_DEBUG("loadState = " << m_loadState << ", something went wrong.");
-  //     return;
-  // }
+  // Perform AI for one RTT after 
+  if (!m_mdFreeze && m_mdTimer.IsRunning()) {
+    AdditiveIncrease(tcb);
+    return;
+  } 
+
+  switch (m_loadState) {
+    case LOAD_LOW:
+      MultiplicativeIncrease(tcb);
+      break;
+    case LOAD_HIGH:
+      AdditiveIncrease(tcb);
+      break;
+    case LOAD_OVERLOAD:
+      MultiplicativeDecrease(tcb);
+      m_mdFreeze = true;
+      m_mdTimer.SetFunction(&Vcp::Noop, this);
+      m_mdTimer.Schedule(Time(m_estInterval * 1000000));
+      return;
+    default:
+      NS_LOG_DEBUG("loadState = " << m_loadState << ", something went wrong.");
+      return;
+  }
 }
 
 void
@@ -143,9 +146,10 @@ Vcp::CongControl(
     const TcpRateOps::TcpRateConnection &rc,
     const TcpRateOps::TcpRateSample &rs)
 {
+  // (VCP) TODO: potential place to do CC
   Time rtt = tcb->m_lastRtt.Get();
 
-  NS_LOG_FUNCTION(this << tcb << rc << rs);
+  NS_LOG_FUNCTION(this << tcb << &rc << &rs);
   NS_LOG_DEBUG("(VCP) tcb->m_cWnd=" << tcb->m_cWnd);
   // // tcb->m_cWnd = 14580;
   // // return;
@@ -162,7 +166,6 @@ Vcp::CongControl(
   // If the load bits are not supported, fall back to TCP New Reno
   if (m_loadState == LOAD_NOT_SUPPORTED) {
     // TODO: What to do if not supported?
-    // TcpNewReno::PktsAcked(tcb, segmentsAcked, rtt);
     // m_cWndFractional = static_cast<double>(tcb->m_cWnd);
     return;
   }
@@ -209,6 +212,7 @@ Vcp::CongControl(
 void
 Vcp::PktsAcked(Ptr<TcpSocketState> tcb, uint32_t segmentsAcked, const Time &rtt)
 {
+  // (VCP) TODO: potential place to do CC
   return;
 
   NS_LOG_FUNCTION(this << tcb << segmentsAcked << rtt);
