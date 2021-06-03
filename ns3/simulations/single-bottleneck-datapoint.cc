1f84b6388d3f8dcdb00e9ebba351cb16516b025c
Mathew Hogan <mathewhogan@DNa1da5e1.SUNet>
2021-06-02 22:35:55 -0700

    Switch to tracing queue pct occupancy

--- ns3/simulations/single-bottleneck-datapoint.cc
+++ ns3/simulations/single-bottleneck-datapoint.cc
@@ -75,7 +75,7 @@ QueueOccupancyTracer (Ptr<OutputStreamWrapper> stream,
                       Ptr<QueueDisc> q)
 {
  *stream->GetStream() << Simulator::Now().GetSeconds () << " "
-                      << q->GetCurrentSize().GetValue() << std::endl;
+                      << (double) q->GetCurrentSize().GetValue() / q->GetCapacity()->GetValue() << std::endl;
   Simulator::Schedule (MilliSeconds(Simulator::Now().GetMilliSeconds() + QUEUE_TRACE_INTERVAL_MS), 
                        &QueueOccupancyTracer,
                        stream,
